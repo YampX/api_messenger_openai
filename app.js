41304a305f5ed591ef22f279ddcf9d7339d67094
YampX <juanpablodriz@gmail.com>
2024-06-04 23:05:05 -0300

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const { ResponseService } = require('./responseService')
+const { ResponseService } = require('./service/ResponseServices')
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
